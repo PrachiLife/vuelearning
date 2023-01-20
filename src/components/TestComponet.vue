5a64ebbc9c4d2ae88a97cedce379695a41e2cb95
PrachiLife <prachi23january@gmail.com>
2023-01-20 18:34:42 +0530

    lifecycle method

--- src/components/TestComponet.vue
+++ src/components/TestComponet.vue
@@ -1,11 +1,43 @@
 <template>
-  <div></div>
+  <div class="">
+    <h1></h1>
+    <input type="text" v-model="data" />
+    <br /><br />
+    <button v-on:click="clearData">Clear</button>
+    <button v-on:click="capsData">CapsData</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TestComponent",
+  data: () => {
+    return {
+      data: null,
+      d: new Date(),
+    };
+  },
+  methods: {
+    clearData() {
+      this.data = null;
+    },
+    capsData() {
+      this.data = this.data.toUpperCase();
+    },
+  },
+  created() {
+    console.log("created");
+    console.log(this.d);
+  },
+  mounted() {
+    console.log("mounted");
+    console.log(this.d);
+  },
+  updated() {
+    console.log("updated");
+    console.log(this.d);
+  },
 };
 </script>
 
-<style scoped></style>
+<style></style>
