8f766a568a80f8da96a919b99a0d6487eebd7ce7
PrachiLife <prachi23january@gmail.com>
2023-01-20 14:02:34 +0530

    clear and caps Data

--- src/components/TestComponet.vue
+++ src/components/TestComponet.vue
@@ -1,11 +1,29 @@
 <template>
-  <div></div>
+  <div class="">
+    <input type="text" v-model="data" />
+    <br /><br />
+    <button v-on:click="clearData">Clear</button>
+    <button v-on:click="capsData">CapsData</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TestComponent",
+  data: () => {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    clearData() {
+      this.data = null;
+    },
+    capsData() {
+      this.data = this.data.toUpperCase();
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style></style>
